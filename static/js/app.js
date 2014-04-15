3e6b7d805dca4705a669b334423ed8ce1227391c
Neil Galvin <neil@galv.in>
2014-04-15 17:49:21 +0100

    Bring back the badger

--- static/js/app.js
+++ static/js/app.js
@@ -9,7 +9,7 @@ L.tileLayer('http://{s}.tile.cloudmade.com/37d359fe74f049e9983dcfb6c8aa7413/997/
 
 L.tileLayer('/tile/{y}/{x}/{z}', {
   minZoom:1,
-  maxZoom:5,
+  maxZoom:6,
   opacity:1.0,
   //attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery ?<a href="http://cloudmade.com">CloudMade</a>'
 }).addTo(map);
