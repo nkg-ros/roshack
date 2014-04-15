fe26ec4381d64a19db246a75d9d6020d0eea3967
Shane Kilkelly <shane@kilkelly.me>
2014-04-15 13:42:07 +0100

    try to return png image from api endpoint

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
+import tile
 
 
 app = Flask(__name__)
 
 
 @app.route('/tile/<string:x>/<string:y>/<string:z>')
-def tile(x, y, z):
-    return "tile %s %s %s" % (x, y, z)
+def get_tile(x, y, z):
+    image = tile.render_image(
+        list(range(256*256))
+    )
+
+    response = make_response(image.tostring())
+    response.headers['Content-Type'] = 'image/png'
+    return response
 
 
 @app.route('/')
