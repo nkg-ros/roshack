e1a6c3538a0ba41b402c592d5ff9a198bf37c443
Shane Kilkelly <shane@kilkelly.me>
2014-04-15 11:20:19 +0100

    add flask service skeleton

---
+++ web_service/service.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/tile/<string:x>/<string:y>/<string:z>')
+def tile(x, y, z):
+    return "tile %s %s %s" % (x, y, z)
+
+    if __name__ == '__main__':
+        app.run(debug=True)
